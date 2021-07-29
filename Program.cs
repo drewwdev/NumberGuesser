8bb0dcec63acb4d8d6243c24d653ea938db8faff
Drew Wilson <jankyy93@gmail.com>
2021-07-28 21:32:18 -0400

    Made a list of 1024 numbers

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NumberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number between 1 and 1024");
+            var numbers = new List<int>() { };
+            for (int number = 1; number <= 1024; number++)
+            {
+                numbers.Add(number);
+            }
+            int halfOfList = numbers.Count / 2;
+            Console.WriteLine($"Is {halfOfList} your number?");
+            Console.WriteLine("If it is your number, type correct. If it is not your number, type higher or lower");
+            var userInput = Console.ReadLine();
+            Console.WriteLine(halfOfList);
+            if (userInput == "correct")
+            {
+                Console.WriteLine("Nice!");
+            }
+            if (userInput == "higher")
+            {
+
+            }
+            if (userInput == "lower")
+            {
+
+            }
+        }
     }
-  }
 }
